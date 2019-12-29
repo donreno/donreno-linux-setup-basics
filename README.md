247ea9b258e352652b9813dc98e26b63bf9756af
Renato Andres San Martín Bustamante <donreno@pop-os.localdomain>
2019-12-29 11:36:41 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,8 @@
+# donreno-linux-setup-basics
+
+Mi script de configuracion para ubuntu (funciona en POP OS)
+
+# Instrucciones
+Correr el script setup.sh, ingresar la password y esperar.
+
+    curl -s https://raw.githubusercontent.com/donreno/donreno-linux-setup-basics/setup.sh | sh
---
+++ setup.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+# Actualizaciones
+echo "Actualizando"
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# Se instala git
+echo "Instalando GIT"
+sudo apt-get -y install git
+
+# Se instala ZSH y Oh My ZSH
+echo "Se instala ZSH"
+sudo apt-get -y install zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+source ~/.zshrc
+
+# Se instala SDK MAN
+echo "Instalando SDK"
+curl -s "https://get.sdkman.io" | bash
+echo "SDK instalado"
+
+# Instalando GraalVM
+echo "Instalando GraalVM"
+echo "Habilitando SDK"
+source ~/.sdkman/bin/sdkman-init.sh
+echo "Inicia instalacion de Graal"
+sdk i java 19.2.1-grl
+sdk d java 19.2.1-grl
+
+# Se instalan las fonts
+echo "Instalando Fonts"
+cd
+mkdir Apps
+cd Apps
+git clone https://github.com/powerline/fonts.git
+cd fonts
+chmod +x install.sh
+./install.sh
+
+# Instalar GO
+echo "Instalando GOLANG"
+sudo add-apt-repository -y ppa:longsleep/golang-backports
+sudo add-apt-repository -y ppa:gophers/archive
+sudo apt-get update
+sudo apt-get -y install golang-go
+
+
+# Instalar Docker
+echo "Instalando Docker"
+sudo apt-get -y install docker
+
+# Instalar vscode
+sudo apt-get -y install code
+
+# Instalar Intellij idea
+sudo add-apt-repository -y ppa:mmk2410/intellij-idea-community
+sudo apt update
+sudo apt install -y intellij-idea-community
+
+# Instalar terminator
+sudo apt-get -y install terminator
+
+# Reminders
+echo
+echo "*****************"
+echo "Recuerda:" 
+echo "- cambiar la fuente del terminal a Source Code Pro for Powerline Medium"
+echo "- instalar gotools en vscode"
